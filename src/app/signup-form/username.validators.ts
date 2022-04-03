72e8e6faf45da384577ec0a088cf8e3d301d90d8
Alkin Maystorov <alkin971@gmail.com>
2022-04-03 21:41:47 +0200

    Add async validator example.

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -6,4 +6,15 @@ export class UsernameValidators {
       return { cannotContainSpace: true };
     return null;
   }
+
+  static shouldBeUnique(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'alkin') resolve({ shouldBeUnique: true });
+        else resolve(null);
+      }, 2000);
+    });
+  }
 }
