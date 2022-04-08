b3736a360b9f74158a39082e68a7cc303f96a5ce
Alkin Maystorov <alkin971@gmail.com>
2022-04-09 01:31:18 +0200

    add: configuring routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,8 +24,14 @@ import { ChangePasswordComponent } from './change-password/change-password.compo
 import { PostsComponent } from './posts/posts.component';
 import { PostService } from './services/post.service';
 import { AppErrorHandler } from './common/app-error-handler';
+
+import { RouterModule } from '@angular/router';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
 import { GithubFollowersService } from './github-followers.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -46,6 +52,10 @@ import { GithubFollowersService } from './github-followers.service';
     ChangePasswordComponent,
     PostsComponent,
     GithubFollowersComponent,
+    NavbarComponent,
+    HomeComponent,
+    GithubProfileComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -53,6 +63,13 @@ import { GithubFollowersService } from './github-followers.service';
     FontAwesomeModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'followers', component: GithubFollowersComponent },
+      { path: 'profile/:username', component: GithubProfileComponent },
+      { path: 'posts', component: PostsComponent },
+      { path: '**', component: NotFoundComponent },
+    ]),
   ],
   providers: [
     CoursesService,
