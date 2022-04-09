8c73ad7ed50b52d9fc2b5dca188b0551ea80213c
Alkin Maystorov <alkin971@gmail.com>
2022-04-09 22:11:24 +0200

    add: query/optional params example

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from '../github-followers.service';
 
 @Component({
@@ -9,9 +10,20 @@ import { GithubFollowersService } from '../github-followers.service';
 export class GithubFollowersComponent implements OnInit {
   followers: any = [];
 
-  constructor(private service: GithubFollowersService) {}
+  constructor(
+    private service: GithubFollowersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {});
+
+    this.route.queryParamMap.subscribe((params) => {});
+
+    // let id = this.route.snapshot.paramMap.get('id');
+    // this.route.queryParamMap.subscribe();
+    // let page = this.route.snapshot.queryParamMap.get('page');
+
     this.service
       .getAll()
       .subscribe((followers) => (this.followers = followers));
